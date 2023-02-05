aad29d6a88c10e8f5aa2066bdc9fe7a221d17b77
thasup <thanachonfirst@hotmail.com>
2023-02-05 21:48:36 +0700

    Fix header typo

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => (
   <div className='app__header app__wrapper section__padding' id='home'>
     <div className='app_wrapper_info'>
       <SubHeading title='Chase the new Favor' />
-      <h1 className='app__header-h1'>The Ket to Fine Dining</h1>
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
       <p className='p__opensans' style={{ margin: '2rem 0' }}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
-      <button type='button' className='custom__buttom'>Explore Menu</button>
+      <button type='button' className='custom__button'>Explore Menu</button>
     </div>
 
     <img src={images.welcome} alt='welcome img' className='app__header-img' />
